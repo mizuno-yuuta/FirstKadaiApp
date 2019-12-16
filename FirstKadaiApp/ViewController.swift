f95bb6f220f7bc5232152267912c772f8e3c7247
yuuta.mizuno <f0b325382t65j6a@icloud.com>
2019-12-16 15:45:44 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by 水野優太 on 2019/12/16.
+//  Copyright © 2019 yuuta.mizuno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
